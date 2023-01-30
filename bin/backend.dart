c087e3d691b3b1d8c6355ac3251d6e9e879a913b
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2023-01-30 17:45:38 -0300

    aula 5

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,14 +1,12 @@
 import 'package:shelf/shelf.dart';
 import 'package:shelf/shelf_io.dart' as shelf_io;
 
+import 'server_handler.dart';
+
 void main() async {
-  final server = await shelf_io.serve(
-      (request) => Response(
-            200,
-            body: 'ok',
-          ),
-      'localhost',
-      8080);
-      
-  print(server);
+   var _server = ServeHandler();
+
+  final server = await shelf_io.serve(_server.handler, 'localhost', 8081);
+
+  print('http://localhost:8081');
 }
