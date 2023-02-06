e9a1e6d20c162d95d1fe3b734bb8ded0803fba33
Leonardo Gonzalez dos Santos <59115521+leozero3@users.noreply.github.com>
2023-02-06 17:50:17 -0300

    aula 8

--- bin/backend.dart
+++ bin/backend.dart
@@ -1,3 +1,9 @@
-void main()async {
-  
+import 'package:shelf/shelf.dart';
+
+import 'api/login_api.dart';
+import 'infra/custom_server.dart';
+
+void main() async {
+  var cascateHandler = Cascade();
+  await CustomServer().initialize(LoginApi().handler);
 }
--- bin/infra/custom_server.dart
+++ bin/infra/custom_server.dart
@@ -1,3 +1,13 @@
-class CustomServer {
+import 'package:shelf/shelf.dart';
+import 'package:shelf/shelf_io.dart' as shelf_io;
 
+class CustomServer {
+  //é o servidor customizado
+  Future<void> initialize(Handler handler) async {
+    String address = 'localhost';
+    int port = 8080;
+
+    await shelf_io.serve(handler, address, port);
+    print('servidor inicializado -> http://$address:$port');
+  }
 }
