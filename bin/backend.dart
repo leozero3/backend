565b535b8d8a1b29ce713197b30c51b85b5996b3
leo <realleo2009@hotmail.com>
2023-02-26 22:11:30 -0300

    aula 17 - inicio

--- bin/backend.dart
+++ bin/backend.dart
@@ -2,20 +2,24 @@ import 'package:shelf/shelf.dart';
 import 'apis/blog_api.dart';
 import 'apis/login_api.dart';
 import 'infra/custom_server.dart';
+import 'infra/middleware_interception.dart';
+import 'infra/security/security_service_imp.dart';
 import 'services/noticia_service.dart';
 import 'utils/custom_env.dart';
 
 void main() async {
-  
   CustomEnv.fromFile('.env-dev');
 
-  var cascadeHandler =
-      Cascade().add(LoginApi().handler).add(BlogApi(NoticiaService()).handler).handler;
+  var cascadeHandler = Cascade()
+      .add(LoginApi(SecurityServiceImp()).handler)
+      .add(BlogApi(NoticiaService()).handler)
+      .handler;
 
-  var handler =
-      Pipeline().addMiddleware(logRequests()).addHandler(cascadeHandler);
+  var handler = Pipeline()
+      .addMiddleware(logRequests())
+      .addMiddleware(MiddlewareInterception().middleware)
+      .addHandler(cascadeHandler);
 
-  
   await CustomServer().initialize(
     handler: handler,
     address: await CustomEnv.get<String>(key: 'server_address'),
